2fb145755fb36eeeb861955b0fa76a44a0559473
juanb86 <juan.e.begnis@gmail.com>
2021-02-23 04:34:22 -0300

    extra3

--- src/index.exercise.js
+++ src/index.exercise.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap-reboot.css'
 import '@reach/dialog/styles.css'
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import {Button, Input, FormGroup} from './components/lib'
+import {Button, Input, FormGroup,Spinner} from './components/lib'
 import {Modal, ModalContents, ModalOpenButton} from './components/modal'
 import {Logo} from './components/logo'
 
@@ -41,7 +41,10 @@ function LoginForm({onSubmit, submitButton}) {
         <label htmlFor="password">Password</label>
         <Input id="password" type="password" />
       </FormGroup>
-      <div>{React.cloneElement(submitButton, {type: 'submit'})}</div>
+      <div>
+        {React.cloneElement(submitButton, {type: 'submit'})}
+        <Spinner />
+      </div>
     </form>
   )
 }
